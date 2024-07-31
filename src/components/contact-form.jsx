de0c4213d56f5caaa3732c62287e4c6e3cf6f775
VladProtchenko <andrii.panasenko295@gmail.com>
2024-07-31 21:31:48 +0300

    made add contact form sticky

--- src/components/contact-form.jsx
+++ src/components/contact-form.jsx
@@ -10,11 +10,14 @@ const initialValue = {
 
 const handleSubmit = (values) => {
   console.log(values);
+  const newContact = {
+
+  }
 };
 
 export default function ContactForm({}) {
   return (
-    <div className='w-full md:max-w-sm'>
+    <div className='w-full md:max-w-sm md:sticky md:top-9 md:left-0'>
       <h2 className='font-medium text-xl'>Create contact</h2>
       <Formik initialValues={initialValue} onSubmit={handleSubmit}>
         <Form className="flex flex-col gap-10">

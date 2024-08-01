b405f5a15add2bd00d8b401c224421469e474062
Andrij-Panasenko <130774640+Andrij-Panasenko@users.noreply.github.com>
2024-08-01 12:44:11 +0000

    Deploying to gh-pages from @ Andrij-Panasenko/contacts-book@c1e06dc00778e73d5ab25da63e34c910f1ce7df0 🚀

---
+++ assets/app-layout-BayZIpzE.js
@@ -0,0 +1,1 @@
+import{j as s,r as t,O as a}from"./index-DTbrbmrC.js";import{I as r}from"./index-CoI9H9cM.js";function n(){return s.jsxs("div",{className:"container mx-auto p-9",children:[s.jsx(t.Suspense,{fallback:null,children:s.jsx(a,{})}),s.jsx(r,{})]})}export{n as default};
--- assets/app-layout-CIlNl29C.js
+++
@@ -1,1 +0,0 @@
-import{j as s,r as t,O as a}from"./index-DEa5tg-K.js";import{I as r}from"./index-DMMIe1I6.js";function n(){return s.jsxs("div",{className:"container mx-auto p-9",children:[s.jsx(t.Suspense,{fallback:null,children:s.jsx(a,{})}),s.jsx(r,{})]})}export{n as default};
